fba617b264978e80a51a565351beecc983c0e756
phwkha <phankhant0098@gmail.com>
2026-03-20 18:25:15 +0700

    refactor: SecurityConfig

--- chatweb_be/src/main/java/com/web/backend/config/SecurityConfig.java
+++ chatweb_be/src/main/java/com/web/backend/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import com.web.backend.jwt.JwtAuthenticationFilter;
 import com.web.backend.service.util.UserServiceDetail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
