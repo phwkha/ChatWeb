82aa7f551e3e37fec212df4ccd755c1eb44bf87c
phwkha <phankhant0098@gmail.com>
2026-07-24 09:50:00 +0700

    refactor: remove unused annotation and string extraction scripts

--- chatweb_be/TestAnno.java
+++
@@ -1,9 +0,0 @@
-@java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-@interface MyAnno {
-    String value();
-}
-
-@MyAnno(TestAnno.MY_VAL)
-public class TestAnno {
-    public static final String MY_VAL = "hello";
-}
--- chatweb_be/TestAnno2.java
+++
@@ -1,9 +0,0 @@
-@java.lang.annotation.Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
-@interface MyAnno {
-    String value();
-}
-
-@MyAnno(MY_VAL)
-public class TestAnno2 {
-    public static final String MY_VAL = "hello";
-}
