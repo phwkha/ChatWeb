b3f6f4e2f1ef1da4b523a772b73c5883f65ea4a4
phwkha <phankhant0098@gmail.com>
2026-07-28 22:25:06 +0700

    feat: Add constructors to NotificationMessageResponse for improved object initialization

--- chatweb_be/src/main/java/com/web/backend/controller/response/NotificationMessageResponse.java
+++ chatweb_be/src/main/java/com/web/backend/controller/response/NotificationMessageResponse.java
@@ -1,11 +1,15 @@
 package com.web.backend.controller.response;
 
 import com.web.backend.common.NotificationsStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotificationMessageResponse {
 
     private NotificationsStatus status;
