7af00d18c4b998317eb949ed7d83f3e6e671a59a
phwkha <phankhant0098@gmail.com>
2026-08-07 19:35:05 +0700

    refactor: Convert EmailPayload class to a record for improved immutability and simplicity

--- chatweb_be/src/main/java/com/web/backend/kafka/payload/EmailPayload.java
+++ chatweb_be/src/main/java/com/web/backend/kafka/payload/EmailPayload.java
@@ -1,27 +1,16 @@
 package com.web.backend.kafka.payload;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class EmailPayload {
+public record EmailPayload(
+    String type,
+    String to,
+    String name,
+    String otp,
+    String subject,
+    String content
+) {
 
     private static final String OTP_STRING = "OTP";
-
     private static final String TEXT_STRING = "TEXT";
-
-    private String type;
-
-    private String to;
-
-    private String name;
-    private String otp;
-
-    private String subject;
-    private String content;
 
     public static EmailPayload createOtpEvent(String to, String name, String otp) {
         return new EmailPayload(OTP_STRING, to, name, otp, null, null);
