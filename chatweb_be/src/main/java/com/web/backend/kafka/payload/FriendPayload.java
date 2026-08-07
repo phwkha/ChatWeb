455383a415d8d12e06d2c677d55e375d05ebc0d8
phwkha <phankhant0098@gmail.com>
2026-08-07 19:35:14 +0700

    feat: Introduce FriendPayload record for managing friend request data

---
+++ chatweb_be/src/main/java/com/web/backend/kafka/payload/FriendPayload.java
@@ -0,0 +1,18 @@
+package com.web.backend.kafka.payload;
+
+import com.web.backend.common.NotificationsType;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+public record FriendPayload(
+        String senderUsername,
+        String senderDisplayName,
+        String recipientUsername,
+        String recipientDisplayName,
+        List<String> recipientUsernames,
+        String destination,
+        NotificationsType senderStatus,
+        NotificationsType recipientStatus) {
+}
